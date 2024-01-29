35d30b7ff46602c4054440a6750c8f97143d3dfb
saniahniah <saniahn548@gmail.con>
2024-01-29 10:11:41 +0700

    menambahkan style css untuk update

--- update.php
+++ update.php
@@ -26,8 +26,57 @@ $row = mysqli_fetch_assoc($result);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Data Tabungan</title>
     <style>
-        .action-buttons .edit-btn {
-    background-color: #28a745;
+  body {
+    font-family: Arial, sans-serif;
+    background-color: #C0C0C0;
+    color: #333;
+    margin: 0;
+    padding: 0;
+}
+
+h2 {
+    font-size: 28px;
+    margin-bottom: 20px;
+    color: #333;
+    text-align: center;
+}
+
+form {
+    max-width: 400px;
+    margin: 20px auto;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+label {
+    display: block;
+    margin-bottom: 10px;
+    color: #666;
+}
+
+input[type="date"],
+input[type="text"],
+button[type="submit"] {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 15px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    box-sizing: border-box;
+}
+
+button[type="submit"] {
+    background-color: #4caf50;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+}
+
+button[type="submit"]:hover {
+    background-color: #43a047;
 }
     </style>
 </head>
